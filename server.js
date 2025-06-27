2d548839043909d6355bf195cb1ab2ca29ed39ab
Paz <paz.giacchino@hotmail.com>
2025-06-26 23:14:16 -0300

    cambio para guardar

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
+
 // Database connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("✅ MongoDB connected"))
